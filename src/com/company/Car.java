2084ec63aaa1ea3c5df9bdd787585a53044bd005
adrozzhin <drozzhinandrei@gmail.com>
2021-11-03 16:27:58 -0700

    the basic structure of the car

---
+++ src/com/company/Car.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Car {
+
+    public String color;
+    public String model;
+    public String name;
+    public int doors;
+    public String engineCapacity;
+}
