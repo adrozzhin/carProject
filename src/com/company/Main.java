38d845c08cf05425fa15bc34157996c0fd465c1c
adrozzhin <drozzhinandrei@gmail.com>
2021-11-03 17:14:26 -0700

    the blue ferrari again

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         Car newCar = new Car();
-        
+
+        newCar.name = "Ferra";
+        newCar.color = "red";
+        newCar.doors = 2;
+        newCar.engineCapacity = "5000cc";
+
+        System.out.println(newCar.color);
     }
 }
